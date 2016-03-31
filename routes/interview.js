c20a83100eb419bddbbf3af629d96d904bd4f960
WorkEric <ruiwmichael@gmail.com>
2016-03-31 17:15:48 -0400

    sort date

--- routes/interview.js
+++ routes/interview.js
@@ -1,11 +1,20 @@
 var router = require('express').Router();
 var mongodb = require('mongodb').MongoClient.connect('mongodb://localhost/l2');
+var _ = require('lodash');
+//var moment = require('moment');
 
-
-router.get('/', function(req, res, next){
-    mongodb.then(function(db){
-        return db.collection('interview').find().toArray();
-    }).then(res.json.bind(res)).catch(next);
+router.get('/', function(req, res, next) {
+    var orderBy = req.params.order || "Date";
+    mongodb.then(function(db) {
+            return db.collection('interview').find().sort({Date: 1}).toArray();
+        })
+    //    .then(function(interviews) {
+    //        return _.sortBy(interviews, function(interview) {
+    //            return new Date(interview[orderBy], "MM/DD/YYYY");
+    //        });
+    //    })
+        
+        .then(res.json.bind(res)).catch(next);
 });
 
 module.exports = router;
