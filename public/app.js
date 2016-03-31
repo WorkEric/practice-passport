f635539cec52435ce0e1774b6d6cd60a5160c49f
WorkEric <ruiwmichael@gmail.com>
2016-03-31 15:38:00 -0400

    angular-ui

--- public/app.js
+++ public/app.js
@@ -1,17 +1,19 @@
-(function(){
+(function() {
     angular.module('app', ['ui.bootstrap'])
-        .controller('it', function($scope, $http, $uibModal) {
-            $http.get('/interview').then(function(data) {
-                $scope.interviews = data.data;
-            });
-
-            $scope.showQuestions = function(interview){
-                $uibModal.open({
-                    templateUrl: '/questions.html',
-                    controller: function($scope){
-                        $scope.questions = interview.questions;
-                    }
+        .controller('it', ['$scope', '$http', '$uibModal',
+            function($scope, $http, $uibModal) {
+                $http.get('/interview').then(function(data) {
+                    $scope.interviews = data.data;
                 });
+
+                $scope.showQuestions = function(interview) {
+                    $uibModal.open({
+                        templateUrl: '/questions.html',
+                        controller: function($scope) {
+                            $scope.questions = interview.questions;
+                        }
+                    });
+                }
             }
-        });
+        ]);
 })();
